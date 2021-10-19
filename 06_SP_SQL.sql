dc74385de1ee7069d2980e42cae5254eda3b79f5
Vladimir Novopashin <novopashinwm@mail.ru>
2021-10-19 22:38:29 +0300

    Add SP SQL

---
+++ 06_SP_SQL.sql
@@ -0,0 +1,17 @@
+﻿USE WideWorldImporters;  
+GO  
+IF OBJECT_ID ( 'Student.uspCustomerAllInfo', 'P' ) IS NOT NULL   
+    DROP PROCEDURE Student.uspCustomerAllInfo;  
+GO  
+CREATE PROCEDURE Student.uspCustomerAllInfo  
+WITH EXECUTE AS CALLER --выполняются от имени вызывающей стороны родительской процедуры 
+AS  
+    SET NOCOUNT ON;  
+    SELECT c.CustomerName AS Customer, ol.StockItemID AS 'Product id'  
+    FROM Sales.Customers AS c   
+    JOIN Sales.Orders AS so   
+      ON so.CustomerID = c.CustomerID   
+    JOIN Sales.OrderLines AS ol   
+      ON ol.OrderID = ol.OrderID  
+    ORDER BY c.CustomerName ASC;   
+GO    
