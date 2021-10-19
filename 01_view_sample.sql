17c4b64c2d9402efc91fef5190bb434d2b9b5e23
Vladimir Novopashin <novopashinwm@mail.ru>
2021-10-19 22:03:11 +0300

    Add view sample

---
+++ 01_view_sample.sql
@@ -0,0 +1,24 @@
+﻿use WideWorldImporters
+go
+DROP VIEW IF EXISTS Website.CustomerDelivery;
+Go 
+
+CREATE VIEW Website.CustomerDelivery 
+AS
+SELECT s.CustomerID,
+       s.CustomerName,      
+       s.PhoneNumber,
+       s.FaxNumber,       
+       s.WebsiteURL,       
+       c.CityName AS CityName,
+       s.DeliveryLocation AS DeliveryLocation,
+       s.DeliveryRun,
+       s.RunPosition
+FROM Sales.Customers AS s
+	LEFT OUTER JOIN [Application].Cities AS c
+	ON s.DeliveryCityID = c.CityID
+
+GO
+
+SELECT *
+FROM Website.CustomerDelivery;
